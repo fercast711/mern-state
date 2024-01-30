ec46df0a214a28efb6328d430b38a3bf8267e12f
fercast711 <fercast7.fc@gmail.com>
2024-01-30 02:23:53 -0600

    conect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
 
 const app = express();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('conection successfully')
+})
 
 app.listen(3000, () => {
-    console.log('listening on port 3000!!');
-}
+        console.log('listening on port 3000!!');
+    }
 );
