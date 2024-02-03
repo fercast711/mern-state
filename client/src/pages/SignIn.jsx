609fb5095fb2aa3c12e45f2996420d2052e0799f
fercast711 <fercast7.fc@gmail.com>
2024-02-03 01:24:26 -0600

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import { authUser } from '../api/auth.api'
+import {useDispatch, useSelector} from 'react-redux'
+import { 
+  signInStart,
+  signInFailure,
+  signSuccess,
+} from '../redux/user/userSlice'
 
 export default function  SignIn() {
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state) => state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -14,16 +20,16 @@ export default function  SignIn() {
     })
   }
   const handleSubmit = async(e) => {
-    setLoading(true);
+    dispatch(signInStart());
     e.preventDefault();
     try {
-      await authUser(formData)
-      setError(null)
+      const res = await authUser(formData)
+      dispatch(signSuccess(res.data))
       navigate('/')
     } catch (error) {
-      setError(error.response.data.message)
+      dispatch(signInFailure(error.response.data.message));
     }
-    setLoading(false)
+
   }
   return (
     <div className='p-3 max-w-lg mx-auto'>
