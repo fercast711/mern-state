acdf5b566a263a223685d69ecf2c24db625692c3
fercast711 <fercast7.fc@gmail.com>
2024-02-05 04:15:15 -0600

    create update user api

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import { errorHandler } from './middleware/errors.middleware.js';
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
 const app = express();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('conection successfully')
@@ -14,6 +16,8 @@ app.listen(3000, () => {
         console.log('listening on port 3000!!');
     }
 );
+
+app.use(cookieParser())
 app.use(cors())
 app.use(express.json());
 app.use('/api/user',userRouter);
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,20 @@
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    try {
+        if(!token){
+            throw createHttpError(401, 'Unauthorized');
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+            if(err) throw createHttpError(403, 'Forbidden')
+            console.log(data)
+            req.user = data;
+            next();
+        })
+    } catch (error) {
+        next(error);
+    }
+}
