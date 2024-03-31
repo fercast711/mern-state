804730a5b33ccb9cd343f4c90386bde315c49f52
fercast711 <fercast7.fc@gmail.com>
2024-03-31 03:40:31 -0600

    created search api route

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { getUser } from "../api/user.api"
 import { Link } from "react-router-dom";
 
-const Contact = ({listing}) => {
+const Contact = ({ listing }) => {
     const [landlord, setLandlord] = useState(null);
     const [message, setMessage] = useState('');
     const handleChange = (e) => {
@@ -20,36 +20,36 @@ const Contact = ({listing}) => {
         }
         fetchLandlord();
     }, [listing.userRef])
-  return (
-    <>
-{landlord && (
-    <div className="flex flex-col gap-2">
-        <p>
-            Contact 
-            <span className="font-semibold"> {landlord.username}</span> for 
-            <span className="font-semibold"> {listing.name.toLowerCase()}</span>
-        </p>
-        <textarea 
-        onChange={handleChange}
-        name="message" 
-        value={message} 
-        id="message" 
-        rows={2}
-        placeholder="Enter you message here..."
-        className="w-full border p-3 rounded-lg"
-        >
+    return (
+        <>
+            {landlord && (
+                <div className="flex flex-col gap-2">
+                    <p>
+                        Contact
+                        <span className="font-semibold"> {landlord.username}</span> for
+                        <span className="font-semibold"> {listing.name.toLowerCase()}</span>
+                    </p>
+                    <textarea
+                        onChange={handleChange}
+                        name="message"
+                        value={message}
+                        id="message"
+                        rows={2}
+                        placeholder="Enter you message here..."
+                        className="w-full border p-3 rounded-lg"
+                    >
 
-        </textarea>
-        <Link 
-        to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-        className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
-        >
-        Send Message
-        </Link>
-    </div>
-)}
-    </>
-  )
+                    </textarea>
+                    <Link
+                        to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+                        className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+                    >
+                        Send Message
+                    </Link>
+                </div>
+            )}
+        </>
+    )
 }
 Contact.propTypes = {
     listing: PropTypes.object.isRequired,
